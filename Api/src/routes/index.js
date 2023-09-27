0492c18b97c8f06bd417163dd9d3c1802a0c8f37
Plushstp <tripkefm@gmail.com>
2023-09-27 01:02:38 -0300

    routes index y products

--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -1,1 +1,20 @@
 //aquí irán las rutas generales, con route.use
+
+const { Router } = require("express");
+
+//Index Routes
+const categoriesRoutes = require("./routes-catgories");
+const clientsRoutes = require("./routes-clients");
+const optionsRoutes = require("./routes-options.js")
+const productsRoutes = require("./routes-product.js")
+const salesRoutes = require("./routes-sales")
+
+const router = Router();
+
+router.use("/categories", categoriesRoutes);
+router.use("/clients", clientsRoutes);
+router.use("/options", optionsRoutes);
+router.use("/products", productsRoutes);
+router.use("/sales", salesRoutes);
+
+module.exports = router;
