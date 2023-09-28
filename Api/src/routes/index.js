a466c5a62ba37e72fa36fb0f8002033053542122
fernando <revcastfer@gmail.com>
2023-09-27 20:26:24 -0500

    correccion

--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -1,15 +1,3 @@
 //aquí irán las rutas generales, con route.use
 
-const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-const categoriesRouter=require('./routes-categories.js');
 
-
-const router = Router();
-
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-router.use("/categories",categoriesRouter);
-
-module.exports = router;
--- Api/src/routes/routes-categories.js
+++ Api/src/routes/routes-categories.js
@@ -1,12 +1,1 @@
 //aqui van las rutas get/post/put/delete correspondientes
-
-const {Router}=require ('express');
-
-let categoriesRouter=Router();
-
-const getCategoriesHandler=require('../handlers/categorieshandlers.js');
-
-
-categoriesRouter.get('/', getCategoriesHandler);
-
-module.exports=categoriesRouter
