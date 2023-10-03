bad130a6b61010d31888c8efe876ff18b8093058
Gus Rodríguez <gorz12@gmail.com>
2023-10-03 15:45:59 -0600

    commit (2) /Mejoras Nav/ Home/Nueva ruta mnto

--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -31,6 +31,7 @@ export default function Navbar() {
         <Link className="navbar-brand" to="/">
           <img src={logoshop} alt="logo" />
         </Link>
+
         <button
           className="navbar-toggler"
           type="button"
@@ -42,6 +43,7 @@ export default function Navbar() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
@@ -49,11 +51,25 @@ export default function Navbar() {
                 Home
               </Link>
             </li>
+
             <li className="nav-item">
               <Link className="nav-link" to="/store">
                 Tienda
               </Link>
             </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/mensaje">
+                Diseños
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/mensaje">
+                Contacto
+              </Link>
+            </li>
+
             <li className="nav-item">
               <select className="form-select" onChange={handleProducts}>
                 <option value="Producto">Productos</option>
@@ -84,13 +100,22 @@ export default function Navbar() {
               </Link>
             </li> */}
             <li className="nav-item">
-              <Link className="nav-link active" to="/Cart">
-                Carrito
+              <Link className="nav-link active" to="/cart">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-cart-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                </svg>
               </Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link active" to="/admin">
-                Administrar
+                Admin
               </Link>
             </li>
           </ul>
