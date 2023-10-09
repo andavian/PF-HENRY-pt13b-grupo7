c506912320619c5a5e0b72c726a1b706a06072dd
Gus Rodríguez <gorz12@gmail.com>
2023-10-09 09:16:26 -0600

    Paginado 5en5

--- Client/src/components/Paginado/Paginado.jsx
+++ Client/src/components/Paginado/Paginado.jsx
@@ -3,15 +3,21 @@ import style from "./Paginado.module.css";
 import Button from "../Paginado/Button/Button";
 
 const Paginado = ({ 
-currentPage, 
-totalPages, 
-onPageChange 
+  currentPage, 
+  totalPages, 
+  onPageChange 
 }) => {
   const pages = [];
 
   for (let i = 1; i <= Math.ceil(totalPages); i++) {
     pages.push(i);
   }
+
+  const pagesPerBlock = 5;
+  const currentBlock = Math.ceil(currentPage / pagesPerBlock);
+
+  const startPage = (currentBlock - 1) * pagesPerBlock + 1;
+  const endPage = Math.min(currentBlock * pagesPerBlock, pages.length);
 
   const handleNext = () => {
     if (currentPage < pages.length) {
@@ -37,7 +43,7 @@ onPageChange
     <div className={style.container}>
       <Button display={true} text="<" onClick={handlePrevious} />
       <ul className={style.list}>
-        {pages.map((nPage) => (
+        {pages.slice(startPage - 1, endPage).map((nPage) => (
           <li key={nPage}>
             <button
               onClick={() => onSpecificPage(nPage)}
@@ -54,3 +60,59 @@ onPageChange
 };
 
 export default Paginado;
+
+
+
+// Paginado original
+
+// const Paginado = ({ 
+//   currentPage, 
+//   totalPages, 
+//   onPageChange 
+//   }) => {
+//     const pages = [];
+//     for (let i = 1; i <= Math.ceil(totalPages); i++) {
+//       pages.push(i);
+//     }
+  
+//     const handleNext = () => {
+//       if (currentPage < pages.length) {
+//         onPageChange(currentPage + 1);
+//       } else {
+//         alert("¡Oh, parece que hemos agotado todas las busquedas disponibles!");
+//       }
+//     };
+  
+//     const handlePrevious = () => {
+//       if (currentPage > 1) {
+//         onPageChange(currentPage - 1);
+//       } else {
+//         alert("¡Oh, parece que hemos agotado todas las busquedas disponibles!");
+//       }
+//     };
+  
+//     const onSpecificPage = (n) => {
+//       onPageChange(n);
+//     };
+  
+//     return (
+//       <div className={style.container}>
+//         <Button display={true} text="<" onClick={handlePrevious} />
+//         <ul className={style.list}>
+//           {pages.map((nPage) => (
+//             <li key={nPage}>
+//               <button
+//                 onClick={() => onSpecificPage(nPage)}
+//                 className={currentPage === nPage ? style.isActive : style.noActive}
+//               >
+//                 {nPage}
+//               </button>
+//             </li>
+//           ))}
+//         </ul>
+//         <Button display={true} text=">" onClick={handleNext} />
+//       </div>
+//     );
+//   };
+  
+//   export default Paginado;
