816affcf01f10581b154d78c9d5118574968f449
MisaBathory05 <tmisa05@gmail.com>
2023-10-20 06:31:38 -0500

    Agregado boton de favoritos y css

--- Client/src/components/ProfileUser/ProfileUser.jsx
+++ Client/src/components/ProfileUser/ProfileUser.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import "./ProfileUser.module.css";
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
+  const [favoriteProducts, setFavoriteProducts] = useState([]); // Estado para productos favoritos
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -10,10 +12,20 @@ const Profile = () => {
 
   return (
     isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
+      <div className="profilecontainer">
+        <div className="profile-dropdown">
+          <img
+            className="profilepicture"
+            src={user.picture}
+            alt={user.name}
+          />
+          <div>
+            <h2 className="profilename">{user.name}</h2>
+            <p className="profileemail">{user.email}</p>
+            <p className="profilecontact">Dirección de envío: 123 Main St, City</p> {/* Agrega más información de contacto si es necesario */}
+            <button>Ver Favoritos 👩‍🚀</button>
+          </div>
+        </div>
       </div>
     )
   );
