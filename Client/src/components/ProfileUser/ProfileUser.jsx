c36ca0caf09865dfd36da12657264aab6e8f4cee
andavian <ing.elbaum@gmail.com>
2023-10-26 22:31:57 -0300

    reparación de bugs

--- Client/src/components/ProfileUser/ProfileUser.jsx
+++ Client/src/components/ProfileUser/ProfileUser.jsx
@@ -1,5 +1,6 @@
 // Importa tus estilos desde el módulo CSS
 import React from "react";
+import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import styles from "./ProfileUser.module.css";
 
@@ -13,35 +14,28 @@ const Profile = () => {
   return (
     isAuthenticated && (
       <div className={styles.container}>
-      <div className={styles.profilecontainer}>
-       
-       <h3 className={styles.profilename}>¡Hola, {user.name}!</h3>
-        <img
-          className={styles.profilepicture}  />
-        
-        <p className={styles.text}>
-          Nos alegra tenerte con nosotros. Queremos asegurarnos de que tu
-          experiencia sea personalizada y completa. Por favor, revisa
-          la información proporcionada a continuación:
-        </p>
+        <div className={styles.profilecontainer}>
+          <h3 className={styles.profilename}>¡Hola, {user.name}!</h3>
+          <img className={styles.profilepicture} />
 
-        <p className={styles.text}>
-        Correo electrónico: 
-        </p>
+          <p className={styles.text}>
+            Nos alegra tenerte con nosotros. Queremos asegurarnos de que tu
+            experiencia sea personalizada y completa. Por favor, revisa la
+            información proporcionada a continuación:
+          </p>
 
-     
-        <h3 className={styles.profileemail}>
-         {user.email}
-        </h3>
-        <p className={styles.text}>
-          Para que tu perfil esté totalmente configurado y disfrutes al máximo
-          de nuestros productos, te invitamos a hacer clic en el siguiente
-          enlace:
-        </p>
-        <a className={styles.button} href="http://localhost:3000/registration">
-          Click aquí para completar tu perfil
-        </a>
-      </div>
+          <p className={styles.text}>Correo electrónico:</p>
+
+          <h3 className={styles.profileemail}>{user.email}</h3>
+          <p className={styles.text}>
+            Para que tu perfil esté totalmente configurado y disfrutes al máximo
+            de nuestros productos, te invitamos a hacer clic en el siguiente
+            enlace:
+          </p>
+          <Link to={"/registration"} className={styles.button}>
+            Click aquí para completar tu perfil
+          </Link>
+        </div>
       </div>
     )
   );
